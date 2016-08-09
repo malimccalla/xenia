a7df9aecbcf628c8ac7cd5752ac3cc9619bb3d9f
lukew244 <luke.webster86@gmail.com>
2016-08-09 11:46:22 +0100

    first passing test

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/new', function(req, res, next) {
+  res.render('users/new', { title: 'Express' });
+});
+
+router.post('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
---
+++ test/features/userRegistrationTest.js
@@ -0,0 +1,28 @@
+var Browser = require('zombie');
+var browser = new Browser();
+var url = 'localhost:3000';
+var chai = require('chai');
+var expect = chai.expect;
+// var app = require('./app.js');
+
+Browser.localhost('localhost', 3000);
+
+
+describe('user registration', function() {
+  before(function(done) {
+    browser.visit('/users/new', done);
+  });
+
+  it('displays an email and password form', function(done) {
+    browser
+      .fill('email', 'test@email.com')
+      .fill('password', '12345')
+      .fill('password-confirmation', '12345')
+      .pressButton('sign-up',done);
+  });
+
+  it('displays', function() {
+    expect(browser.html('p')).to.include('Welcome');
+  });
+
+});
