3298f8a09bfa30891d8f42a881aa68a809291642
Jack Oddy <jackoddy91@gmail.com>
2016-08-10 11:17:19 +0100

    actually works now. Adding spaces adds to database - can retreieve and pass to view and tests actually test the things

--- test/features/listSpacesTest.js
+++ test/features/listSpacesTest.js
@@ -9,19 +9,20 @@ Browser.localhost('localhost', 3000);
 
 describe('Listing Spaces', function() {
   before(function(done) {
+    console.log(done);
     browser.visit('/spaces/new', done);
-  });
+    });
 
-  it('filling out the form to add a space', function(done) {
-    browser
-      .fill('name', 'Cute Cottage')
-      .fill('description', 'Romantic getaway for two')
-      .fill('price', 230)
-      .pressButton('List space', done);
+    describe('user can list spaces', function () {
+      before(function(done) {
+        browser
+          .fill('name', 'House')
+          .fill('description', 'Romantic getaway for two')
+          .fill('price', 230)
+          .pressButton('List space', done);
+      });
+      it('adds space to the list of spaces', function() {
+        expect(browser.html('.spaces')).to.include('House');
+      });
+    });
   });
-
-  it('adds space to the list of spaces', function() {
-    expect(browser.html('.spaces')).to.include('Cute Cottage');
-  });
-
-});
